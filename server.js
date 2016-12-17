6625386bd7ae8ba660bcb9d4f2237f76dd1a508e
Lenin Capa <leviskp87@gmail.com>
2016-12-17 17:01:01 -0500

    add gzippo

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var http = require('http');
+var gzippo = require('gzippo');
+
 var app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/app'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // make sure index is in the right directory. In this case /app/index.html
-    res.render('index');
+app.use(gzippo.staticGzip('' + __dirname));
+app.use('/*', function(req, res){
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+var server = http.createServer(app);
+server.listen(process.env.PORT || 5000);
