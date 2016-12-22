030937b4560a1f1e8a00a25e30722a1e468d5a05
Lenin Capa <leviskp87@gmail.com>
2016-12-22 16:14:54 -0500

    fix path images deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 5000;
 var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', function(req, res){
+app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
